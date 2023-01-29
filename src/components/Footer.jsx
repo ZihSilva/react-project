01027b79824917464957782eac63d259f52c5cba
zisilva <zimarlensilva@gmail.com>
2023-01-28 20:50:16 -0800

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <h1>Hello</h1>
+    </div>
+  );
+}
+export default App;
